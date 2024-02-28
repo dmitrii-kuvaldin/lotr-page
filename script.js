c9aba8e082ec2c9afaa6394081018d12310f9ef9
mohnapne <kuvaldin2012@gmail.com>
2024-02-28 19:42:24 +0100

    upd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const filmTitle = document.getElementById("title")
+const filmLink = document.getElementById("link")
 const filmOscar = document.getElementById("oscar")
 const filmDescription = document.getElementById("description")
 const filmPoster = document.getElementById("poster")
@@ -10,13 +11,16 @@ async function getData() {
     "https://dmitrii-kuvaldin.github.io/lotr-api/data.json",
   )
   const data = await res.json()
-  const { title, year, hasOscar, poster, description } = data
+  const { title, year, wiki, hasOscar, poster, description } = data
 
   // добавление данных к текстовому элементу через textContent
   filmTitle.textContent = `${title}, ${year} year`
   filmPoster.src = poster
   filmDescription.textContent = description
-  filmOscar.textContent = hasOscar ? 'Won 11 oscars ✨' : 'Now awards :('
+  filmOscar.textContent = hasOscar ? "Won 11 oscars ✨" : "Now awards :("
+  filmLink.textContent = "Ссылка на wiki"
+  filmLink.href = wiki
+  filmLink.target = "_blank"
 }
 
 getData()
